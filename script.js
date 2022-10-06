2d06a104451959043b49d7ca87b6632e26f86ce6
EReaX <67808914+EReaXe@users.noreply.github.com>
2022-10-06 19:19:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-
-//       if ("serviceWorker" in navigator) {
-//         window.addEventListener("load", function () {
-//           navigator.serviceWorker.register("./sw.js");
-//         });
-//       }
-
-
 (function () {
   const second = 1000,
     minute = second * 60,
     hour = minute * 60,
-    day = hour * 24,
-    month = day * 30;
+    day = hour * 24;
 
   let new_year = "June 18, 2023 08:00:00",
     countDown = new Date(new_year).getTime(),
@@ -19,12 +10,7 @@
       let now = new Date().getTime(),
         distance = countDown - now;
 
-      (document.getElementById("months").innerText = Math.floor(
-        distance / month
-      )),
-        (document.getElementById("days").innerText = Math.floor(
-          (distance % month) / day
-        )),
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
         (document.getElementById("hours").innerText = Math.floor(
           (distance % day) / hour
         )),
@@ -41,7 +27,7 @@
           countdown = document.getElementById("countdown"),
           content = document.getElementById("content");
 
-        headline.innerText = "Seçim Günü 🎉 aaa";
+        headline.innerText = "Seçim Günü 🎉";
         countdown.style.display = "none";
         content.style.display = "block";
 
