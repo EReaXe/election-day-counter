6205c6f2c31f80c0e4a6c7a934579e0213f678a1
EReaX <67808914+EReaXe@users.noreply.github.com>
2024-05-09 22:48:47 +0300

    Update timer.js

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,7 +1,7 @@
 // Seçim tarihini burada ayarlayın
-var electionDate1 = new Date("March 31, 2024 08:00:00");
+var electionDate1 = new Date("May 28, 2028 08:00:00");
 var electionDate2 = new Date("May 28, 2028 08:00:00");
-var electionDate3 = new Date("May 28, 2028 08:00:00");
+var electionDate3 = new Date("March 31, 2029 08:00:00");
 
 // A tanımlanan sayaç öğelerine referans
 var daysElemA = document.getElementById("daysA");
